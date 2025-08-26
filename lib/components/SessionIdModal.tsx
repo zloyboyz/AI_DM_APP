c4ab9a4658ecac3b26d3a8a64e2f36883f3473aa
zloyboyz <121572341+zloyboyz@users.noreply.github.com>
2025-08-26 21:33:14 +0300

    Fix SessionIdModal.tsx syntax error

--- lib/components/SessionIdModal.tsx
+++
@@ -1,5 +0,0 @@
-@@ .. @@
- import { LinearGradient } from 'expo-linear-gradient';
- import { X } from 'lucide-react-native';
--import { getSessionId } from '../lib/session';
-+import { getSessionId } from '../session';
