dbe64fce05d0fad8512e5ff6d3d7648c9c4d42a5
zloyboyz <121572341+zloyboyz@users.noreply.github.com>
2025-08-26 21:13:36 +0300

    Updated useFrameworkReady.ts

--- lib/hooks/useFrameworkReady.ts
+++ lib/hooks/useFrameworkReady.ts
@@ -1,20 +1,13 @@
-@@ .. @@
- import { useEffect } from 'react';
--import { SplashScreen } from 'expo-splash-screen';
- 
- declare global {
-   interface Window {
-@@ -8,9 +7,4 @@ declare global {
- }
- 
- export function useFrameworkReady() {
--  useEffect(() => {
--    // Hide splash screen when framework is ready
--    SplashScreen?.hideAsync()?.catch(() => {
--      // Ignore errors if splash screen is already hidden
--    });
--  });
-+  useEffect(() => {
-+    window.frameworkReady?.()
-+  })
- }
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    frameworkReady?: () => void;
+  }
+}
+
+export function useFrameworkReady() {
+  useEffect(() => {
+    window.frameworkReady?.();
+  });
+}
