3c54e9cacfda459873679bf3c711437423c05a77
zloyboyz <121572341+zloyboyz@users.noreply.github.com>
2025-09-05 18:46:27 +0300

    Fix webhook and polyfill errors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import 'react-native-url-polyfill/auto';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import React, { useCallback, useEffect, useState } from 'react';
